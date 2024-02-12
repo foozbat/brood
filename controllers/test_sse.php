c04a38c4d58c5935a363ab7532d92f1b21076368
foozbat <github@foozbat.net>
2024-02-12 19:47:37 +0000

    sse updates

--- controllers/test_sse.php
+++ controllers/test_sse.php
@@ -1,8 +1,6 @@
 <?php
-date_default_timezone_set("America/New_York");
-header("X-Accel-Buffering: no");
-header("Content-Type: text/event-stream");
-header("Cache-Control: no-cache");
+
+use Fzb;
 
 $pub = new Redis();
 $sub = new Redis();
@@ -12,34 +10,25 @@ $sub->setOption(Redis::OPT_READ_TIMEOUT, 10);
 
 $pub->publish('chat', "client connected, PID:".getmypid());
 
-while (!connection_aborted()) {
+$sse = new Fzb\SSE();
+$sse->stream(function () use ($pub, $sub, $sse) {
     try {
-        $sub->subscribe(['chat'], function ($sub, $channel, $message) use ($pub) {
+        $sub->subscribe(['chat'], function ($sub, $channel, $message) use ($pub, $sse) {
             if (str_contains($message, "client connected")) {
                 echo "SAW SOMEONE";
                 $pub->publish('chat', "PID:".getmypid()." saw someone connect");
             }
 
             if (str_contains($message, 'chat-message')) {
-                send_message($message, rand(1000000,2000000), "something");
+                $sse->message($message, "something");
             }
         });
     } catch (RedisException $e) {
         // on sub read timeout, send ping
-        send_message('ping', rand(1000000,2000000), 'ping');
+        $sse->message('ping', 'ping');
     }
-}
+});
 
 $pub->publish('chat', "client disconnected, PID:".getmypid());
 $pub->close();
 $sub->close();
-echo "closed";
-
-function send_message($message, $id, $data) {
-    echo "event: $message" . PHP_EOL;
-    echo "data: {}" . PHP_EOL;
-    echo PHP_EOL;
-
-    ob_end_flush();
-    flush();
-}
